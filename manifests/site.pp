58f96717ce493e19fbe7dc508d0dd567d801241d
zhasaan <zhasaan@gmail.com>
2019-04-22 15:15:47 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
    ensure  => file,
-   content => 'This is README file',
+   content => 'This is README file of control repo',
    owner   => 'root',
    mode    => '0644',
   }
