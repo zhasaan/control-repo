89abc579611762cdc47135b990809217815b8d22
zhasaan <zhasaan@gmail.com>
2019-04-22 19:10:06 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,9 +2,9 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
-  file {'/root/README':
-   ensure => 'file',
-   contant => $fqdn,
+  file { '/root/README':
+   ensure =>  'file',
+   content => $fqdn,
   }
 }
 node /^web/ {
